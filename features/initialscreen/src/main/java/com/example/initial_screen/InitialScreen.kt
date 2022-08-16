04192fc81728e2a55b61f41a398200152772a5d8
shimoju <soba.ha.kenkou@gmail.com>
2022-08-16 15:43:09 +0900

    test BoxWithConstraints

--- features/initialscreen/src/main/java/com/example/initial_screen/InitialScreen.kt
+++ features/initialscreen/src/main/java/com/example/initial_screen/InitialScreen.kt
@@ -1,13 +1,17 @@
 package com.example.initial_screen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
@@ -15,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -37,6 +42,11 @@ fun InitialScreenRoute(
         onClickCreateAccount = {},
         onClickSignIn = {}
     )
+    Box(modifier = Modifier.size(200.dp)) {
+        BoxWithConstraints(modifier = Modifier.fillMaxSize().background(color = Color.Cyan)) {
+            print("BBBBBBBBBBBBBBBBBBBB max:${maxWidth} / min:${minWidth}")
+        }
+    }
 }
 
 @Composable
