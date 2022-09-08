2cd574b1992770188d16338b058bb041970848c0
shimoju <soba.ha.kenkou@gmail.com>
2022-09-08 17:02:28 +0900

    add screenshot test

--- features/initialscreen/src/main/java/com/example/initial_screen/InitialScreen.kt
+++ features/initialscreen/src/main/java/com/example/initial_screen/InitialScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
@@ -118,4 +119,10 @@ fun Logo(
         model = "https://pbs.twimg.com/profile_banners/1072657247388323840/1649466745/1500x500",
         contentDescription = "logo"
     )
+}
+
+@Preview
+@Composable
+fun test() {
+    Text(text = "TEST")
 }
