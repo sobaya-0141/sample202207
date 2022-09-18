52452a768d147e509b72551afe7b22c83860364d
sobaya-0141 <soba.ha.kenkou@gmail.com>
2022-09-18 10:27:14 +0900

    add constraintlayout sample

--- app/src/main/java/com/example/sample202207/menuscreen/MenuScreen.kt
+++ app/src/main/java/com/example/sample202207/menuscreen/MenuScreen.kt
@@ -2,12 +2,14 @@ package com.example.sample202207.menuscreen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -15,6 +17,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
 
 @Composable
@@ -55,5 +59,28 @@ internal fun MenuScreen(
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }
+ConstraintLayout {
+    val (box, text) = createRefs()
+    Box(modifier = Modifier
+        .width(200.dp)
+        .height(40.dp)
+        .background(color = Color.Blue)
+        .constrainAs(box) {
+            start.linkTo(parent.end)
+            end.linkTo(parent.start)
+        }
+    ) {}
+    Text(
+        modifier = Modifier
+            .background(Color.Red)
+            .constrainAs(text) {
+                start.linkTo(box.start)
+                end.linkTo(box.end)
+                top.linkTo(box.bottom)
+                width = Dimension.fillToConstraints
+            },
+        text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    )
+}
     }
 }
